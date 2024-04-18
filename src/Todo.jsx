c47c3080aa3fa8495879713b9132b5c8d5c053f5
fadayopaul <paulfadayo@yahoo.com>
2024-04-18 01:24:50 +0100

    style: :art: Update UI implementation

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 function Todo() {
   const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
 
   async function getTodoData() {
     await axios
@@ -16,25 +17,55 @@ function Todo() {
     getTodoData();
   }, []);
 
-  console.log(data);
+  console.log(show);
 
   return (
     <main>
-      <div className="h-screen bg-[#fce3c2] p-10">
-        <div className="box">
-          <div className="border-b-4 border-black py-6 text-center">
-            <h1>Things to do</h1>
+      <div className="h-screen bg-[#F6A89E] p-10">
+        <div className="flex flex-col gap-9">
+          <div className="box">
+            <div className="border-b-4 border-black py-6 text-center">
+              <h1 className="font-primary text-4xl font-extrabold">
+                Things to do
+              </h1>
+            </div>
+
+            <div className="p-5">
+              <ul>
+                {data.map((items) => (
+                  <li key={items.id}>
+                    <input
+                      type="checkbox"
+                      className="checkbox peer rounded-md border-gray-300 text-[#F6A89E] focus:ring-orange-300"
+                    />
+                    <label className="pl-3 ">{items.task}</label>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
 
-          <div className="p-5 ">
-            <ul>
-              {data.map((items) => (
-                <li key={items.id}>
-                  <input type="checkbox" />{" "}
-                  <span className="pl-2">{items.task}</span>
-                </li>
-              ))}
-            </ul>
+          <div className="textbox">
+            <div onClick={() => setShow((prev) => !prev)} className="py-3">
+              <p className="font-primary inline-block cursor-pointer rounded-full bg-[#33322E] px-3.5 py-1 text-4xl font-medium text-white">
+                +
+              </p>
+            </div>
+
+            {show && (
+              <form>
+                <div className="flex gap-5">
+                  <input
+                    type="text"
+                    className="textInput w-full rounded-lg border-none outline-none"
+                  />
+
+                  <button className="font-primary rounded-3xl bg-[#33322E] px-6 py-2 text-white">
+                    Send
+                  </button>
+                </div>
+              </form>
+            )}
           </div>
         </div>
       </div>
